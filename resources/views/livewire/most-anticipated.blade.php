3cd8d8af483f367cede7f9599e274dd8c9a0cc1b
Arturo Macias <arturomaciasmx@gmail.com>
2020-09-16 18:46:13 -0700

    Skaleton loader

--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -10,9 +10,14 @@
             </div>
         </div>
         @empty
-        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        </svg>
+        @foreach (range(1,4) as $game)
+            <div class="game flex">
+                <div class="w-12 h-16 bg-gray-700"></div>
+                <div class="ml-4">
+                    <div class="text-transparent bg-gray-600">Game title goes here</div>
+                    <p class="text-transparent text-sm bg-gray-600 mt-2 inline-block">00/00/00</p>
+                </div>
+            </div>
+        @endforeach
     @endforelse
 </div> <!-- End most-anticipated -->
--- resources/views/livewire/recently-reviewed.blade.php
+++ resources/views/livewire/recently-reviewed.blade.php
@@ -1,8 +1,8 @@
 <div wire:init="loadRecentlyReviewed" class="recently-reviewed w-full lg:w-3/4 lg:mr-32">
     <h2 class="text-blue-500 uppercase tracking-width font-semibold">Recently Reviewed</h2>
     <div class="recently-reviewed-container space-y-12 mt-8">
-        @forelse ($recentlyReviewed as $game)
-            
+
+        @forelse ($recentlyReviewed as $game)            
         <div class="game bg-gray-800 rounded-lg flex p-6">
             <div class="relative flex-none">
                 <a href="#">
@@ -30,10 +30,22 @@
         </div>
 
         @empty
-        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        </svg>
+        @foreach (range(1,3) as $game)
+            <div class="game bg-gray-800 rounded-lg flex p-6">
+                <div class="relative flex-none">
+                    <div class="w-44 h-56 bg-gray-700"></div>
+                </div>
+                <div class="ml-12">
+                    <div class="text-transparent bg-gray-600 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing</div>
+                    <div class="text-transparent bg-gray-600 mt-2 inline-block">PS4, PC, Switch, X-box</div> 
+                    <div class="mt-8">
+                        <div class="bg-gray-600 text-transparent mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, soluta.</div>
+                        <div class="bg-gray-600 text-transparent mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, soluta.</div>
+                        <div class="bg-gray-600 text-transparent mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, soluta.</div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
         @endforelse
         
     </div>
